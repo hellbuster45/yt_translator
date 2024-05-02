8d9260ebf5472e1cdc1053d27d5dcf7f87289d7c
hellbuster45 <rupenkumar22@gmail.com>
2024-05-03 02:55:53 +0530

    finally done....

--- transcripter.py
+++ transcripter.py
@@ -1,23 +1,5 @@
 import assemblyai as aai
 import data as d
-# # API key
-# aai.settings.api_key = d.auth_key
-
-# # File URL
-# # FILE_URL = "videos\jpen.mp4"
-# FILE_URL = r"videos\LowTierGod Open Verse Challenge meme.mp4"
-
-# config = aai.TranscriptionConfig(language_detection=True)
-# transcriber = aai.Transcriber(config=config)
-
-# transcript = transcriber.transcribe(FILE_URL, config=config)
-
-# if transcript.status == aai.TranscriptStatus.error:
-#     print(transcript.error)
-# else:
-#     print(transcript.text)
-#     with open('response.txt', mode='w', encoding= 'utf-8') as f:
-#         f.write(transcript.export_subtitles_srt())
 
 class transcribe:
     def __init__(this, url, lang_code):
